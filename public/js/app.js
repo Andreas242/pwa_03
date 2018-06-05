14aaae86a748da6c15d0ec9dbe49f3f1572daf1d
AHLGREN Andreas <aahlgren@one.steria.dom>
2018-06-05 12:56:37 +0200

    removed secret stuff

---
+++ public/js/app.js
@@ -0,0 +1,5 @@
+/*
+You need to register the serviceworker 
+To see if you have access to a SW you can check for 'serviceWorker' in navigator
+
+*/
---
+++ public/sw.js
@@ -0,0 +1,8 @@
+/* add serviceworker code here 
+   A servcieworker only listens to events and is refered to as 'self' 
+   ie. self.addEventListener('eventname', (event) =>) {
+
+   }
+
+   you need to *install* the service worker, *activate* it and probably see that you can intercept *fetch*-requests
+*/
